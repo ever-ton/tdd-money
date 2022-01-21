ece25eec02fbf1c7f15254a9edcdc3924b8e4b17
Everton Bezerra <evertonbezerra_93@hotmail.com>
2022-01-20 21:12:29 -0300

    refactor: remove dollar and euro classes

--- src/money.ts
+++ src/money.ts
@@ -7,12 +7,12 @@ export class Money {
       this._currency = currency
     }
 
-    static dollar (amount: number): Dollar {
-      return new Dollar(amount, 'USD')
+    static dollar (amount: number): Money {
+      return new Money(amount, 'USD')
     }
 
-    static euro (amount: number): Euro {
-      return new Euro(amount, 'EUR')
+    static euro (amount: number): Money {
+      return new Money(amount, 'EUR')
     }
 
     equals (other: Money): boolean {
@@ -30,10 +30,4 @@ export class Money {
     currency (): string {
       return this._currency
     }
-}
-
-export class Dollar extends Money {
-}
-
-export class Euro extends Money {
 }
